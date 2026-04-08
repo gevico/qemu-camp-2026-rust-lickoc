9c8a386d412b27e0143a263bbb9cbba3957622d9
lickoc <1048796996@qq.com>
2026-04-08 13:10:49 +0800

    update 4.8

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,7 +1,7 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -12,7 +12,8 @@ pub fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+    // Execute `rustlings hint iterators4` for h    ints.
+    (1..=num).into_iter().fold(1, |acc,x|acc * x)
 }
 
 #[cfg(test)]
